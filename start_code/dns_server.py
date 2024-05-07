97523dfb54eee2ba96194c1339fd57194a134ef5
Lianting Wang <wanglianting@vip.qq.com>
2024-05-08 01:10:14 +0800

    modify 3

--- start_code/dns_server.py
+++ start_code/dns_server.py
@@ -1,7 +1,20 @@
 from scapy.all import *
 import socket
+import configparser
+import logging
 
-def forward_dns_query(data, server='8.8.8.8', port=53):
+config = configparser.ConfigParser()
+config.read('/home/mininet/Captive-Portal/config.ini')
+DNS_Server = config['DEFAULT']['DNS_Server']
+DNS_Server_port = config['DEFAULT']['DNS_Server_port']
+internet_ip = config['DEFAULT']['internet_ip']
+captive_portal_ip = config['DEFAULT']['captive_portal_ip']
+dns_server_log = config['DEFAULT']['dns_server_log']
+
+# Setup logging
+logging.basicConfig(filename=dns_server_log, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def forward_dns_query(data, server=DNS_Server, port=int(DNS_Server_port)):
     """Forward DNS query to a specified DNS server and return the response."""
     # TODO: Implement the logic to forward DNS queries to the specified DNS server
     pass
@@ -12,4 +25,5 @@ def dns_interceptor(packet):
     pass
 
 # Start the DNS interceptor
+logging.info("DNS Interceptor setup complete. Starting packet sniffing...")
 # TODO: Start sniffing UDP traffic on port 53 and pass intercepted packets to the dns_interceptor function
